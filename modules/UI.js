094afcfea2c7039524e0bb433a6592ea5fe90fb0
mosidrum <mosiokanga@gmail.com>
2023-05-01 18:41:15 +0100

    Refactor code

---
+++ modules/UI.js
@@ -0,0 +1,33 @@
+import { Store } from "./store.js";
+
+export class UI {
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector('.container');
+    const inside = document.createElement('ul');
+    inside.classList = 'book-holder';
+    inside.innerHTML += `
+      <p>${book.title}</p> <p class='by'>by</p> <p>${book.author}</p>
+      <button class="delete">Remove</button>
+     `;
+    list.appendChild(inside);
+  }
+
+  static clear() {
+    const title = document.querySelector('#title');
+    const author = document.querySelector('#author');
+    title.value = '';
+    author.value = '';
+  }
+
+  static deleteBook(target) {
+    if (target.classList.contains('delete')) {
+      target.parentElement.remove();
+    }
+  }
+}
